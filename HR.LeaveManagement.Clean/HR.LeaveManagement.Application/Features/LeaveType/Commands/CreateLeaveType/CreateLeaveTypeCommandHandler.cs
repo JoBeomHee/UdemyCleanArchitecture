bdc489657208027c501076b12584fa552bff3984
JoBeomHee <afsdzvcx123@gmail.com>
2024-01-17 20:08:41 +0900

    CQRS, FluentValidator 추가

---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+
+public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, int>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public CreateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    { 
+        this._mapper = mapper;
+        this._leaveTypeRepository = leaveTypeRepository;
+    }
+
+    public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+    {
+        // 들어오는 데이터 유효성 검가
+        var validator = new CreateLeaveTypeCommandValidator(_leaveTypeRepository);
+        var validationResult = await validator.ValidateAsync(request);
+
+        if (validationResult.Errors.Any())
+            throw new BadRequestException("Invalid LeaveType", validationResult);
+
+        // Object -> Entity Convert
+        var leaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
+
+        // Database 추가
+        await _leaveTypeRepository.CreateAsync(leaveTypeToCreate);
+
+        // id 반환
+        return leaveTypeToCreate.Id;
+    }
+}
